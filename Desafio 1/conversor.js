376b786616bda3e63a8c2766a0db4a9d790cc1ef
Aline Santos <alinesantos.zoe92@gmail.com>
2021-03-29 13:28:05 -0300

    Enviar git

---
+++ Desafio 1/conversor.js
@@ -0,0 +1,14 @@
+var valorEmDolarTexto = prompt("Informe o valor em dolar")
+
+var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+
+var valorEmReal = valorEmDolarNumero * 5.78
+var valorEmRealFixado = valorEmReal.toFixed(2)
+
+alert(valorEmRealFixado)
+
+// é para eu colocar comentarios no codigo
+
+// Revisão
+// var == variaveis 
+// alert - parseInt - parseFloat - prompt
